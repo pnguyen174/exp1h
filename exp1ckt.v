762a50d7c1739c31bcafed711578f78149a64141
tung-aiden <115751756+tung-aiden@users.noreply.github.com>
2023-04-09 21:48:20 -0700

    Update exp1ckt.v

--- exp1ckt.v
+++ exp1ckt.v
@@ -15,11 +15,13 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module exp1ckt(input BTN, 
-               input clk, 
-               output logic [3:0]An, 
-               output logic [7:0]seg, 
-               output logic [3:0]led);
+module exp1ckt(
+  input BTN, 
+  input clk, 
+  output logic [3:0]An, 
+  output logic [7:0]seg, 
+  output logic [3:0]led
+  );
    
 // wires 
 logic up1;
@@ -44,9 +46,10 @@ assign led = toRAM; // displaying address of RAM to LED
 assign avg = {1'b0,sum[7:1]}; // bit slicing to average two values (rounding up)
 
 clk_2n_div_test #(.n(24)) MY_DIV ( 
-          .clockin   (clk), 
-          .fclk_only (0),          
-          .clockout  (clk_div)   );  
+     .clockin   (clk), 
+     .fclk_only (0),          
+     .clockout  (clk_div)  
+     );  
 
 cntr_up_clr_nb #(.n(4)) MY_CNTR1 ( // counter for ROM's
      .clk   (clk_div), 
@@ -55,7 +58,8 @@ cntr_up_clr_nb #(.n(4)) MY_CNTR1 ( // counter for ROM's
      .ld    (0), 
      .D     (0), 
      .count (toROM), 
-     .rco   (RCO1)   ); 
+     .rco   (RCO1)   
+     ); 
      
 cntr_up_clr_nb #(.n(5)) MY_CNTR2 ( // counter for tracking valid values
      .clk   (clk_div), 
@@ -64,7 +68,8 @@ cntr_up_clr_nb #(.n(5)) MY_CNTR2 ( // counter for tracking valid values
      .ld    (0), 
      .D     (0), 
      .count (count2out), 
-     .rco   ()   ); 
+     .rco   ()   
+     ); 
      
 cntr_up_clr_nb #(.n(4)) MY_CNTR3 ( // counter for cycling thorugh RAM address
      .clk   (clk_div), 
@@ -73,33 +78,36 @@ cntr_up_clr_nb #(.n(4)) MY_CNTR3 ( // counter for cycling thorugh RAM address
      .ld    (0), 
      .D     (0), 
      .count (toRAM), 
-     .rco   ()   ); 
+     .rco   ()   
+     ); 
  
 ROM_16x8a my_ROM (
-      .addr  (toROM),  
-      .data  (ROM1out),  
-      .rd_en (1)    );
+     .addr  (toROM),  
+     .data  (ROM1out),  
+     .rd_en (1)    
+     );
 
 ROM_16x8b my_ROM2 (
       .addr  (toROM),  
       .data  (ROM2out),  
-      .rd_en (1)    );
+      .rd_en (1)    
+      );
  
  rca_nb #(.n(8)) MY_RCA (
-          .a   (ROM1out), 
-          .b   (ROM2out), 
-          .cin (8'b00000001), // adding 1 as carry-in allows for rounding up when bit slicing
-          .sum (sum), 
-          .co  ()
-          );
+      .a   (ROM1out), 
+      .b   (ROM2out), 
+      .cin (8'b00000001), // adding 1 as carry-in allows for rounding up when bit slicing
+      .sum (sum), 
+      .co  ()
+      );
  
  comp_nb #(.n(8)) MY_COMP (
-          .a  (avg), 
-          .b  (8'b00001111), 
-          .eq (), 
-          .gt (GT),
-          .lt ()
-          );  
+      .a  (avg), 
+      .b  (8'b00001111), 
+      .eq (), 
+      .gt (GT),
+      .lt ()
+      );  
 
  ram_single_port #(.n(4),.m(8)) my_ram (
       .data_in  (avg), 
@@ -110,26 +118,28 @@ ROM_16x8b my_ROM2 (
       );  
       
    univ_sseg my_univ_sseg (
-     .cnt1    ({6'b000000, displayRAM}), 
-     .cnt2    ({3'b000, count2out}), 
-     .valid   (1), 
-     .dp_en   (0), 
-     .dp_sel  (2'b00), 
-     .mod_sel (2'b01), 
-     .sign    (0), 
-     .clk     (clk), 
-     .ssegs   (seg), 
-     .disp_en (An)    ); 
+      .cnt1    ({6'b000000, displayRAM}), 
+      .cnt2    ({3'b000, count2out}), 
+      .valid   (1), 
+      .dp_en   (0), 
+      .dp_sel  (2'b00), 
+      .mod_sel (2'b01), 
+      .sign    (0), 
+      .clk     (clk), 
+      .ssegs   (seg), 
+      .disp_en (An)
+      ); 
    
    fsm_template FSM (
-     .BTN(BTN),
-     .RCO(RCO1),
-     .GT(GT),
-     .clk(clk_div),
-     .up(up1),
-     .up2(up2),
-     .up3(up3),
-     .we(WE),
-     .clr(clr));
+      .BTN(BTN),
+      .RCO(RCO1),
+      .GT(GT),
+      .clk(clk_div),
+      .up(up1),
+      .up2(up2),
+      .up3(up3),
+      .we(WE),
+      .clr(clr)
+      );
 
 endmodule
